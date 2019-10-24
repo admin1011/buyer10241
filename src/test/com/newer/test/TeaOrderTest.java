6561116694389c3e126cf83a265c576721fe2709
admin1011 <1323639280@qq.com>
2019-10-24 10:52:35 +0800

    Initial commit

---
+++ src/test/com/newer/test/TeaOrderTest.java
@@ -0,0 +1,16 @@
+package com.newer.test;
+
+import org.junit.Test;
+
+/**
+ * @author shining
+ */
+public class TeaOrderTest {
+
+    @Test
+    public void test(){
+
+        System.out.println("ceshiceshi");
+
+    }
+}
